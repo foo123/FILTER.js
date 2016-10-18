57381f87dc9429859a0f8374b7dbdbd4fbe082b0
Nikos M <nikosms0@gmail.com>
2016-10-18 14:27:37 +0300

    css-sprite-animation generator example fixed

--- examples/node/sprites/css-sprite-animation.js
+++ examples/node/sprites/css-sprite-animation.js
@@ -3,37 +3,44 @@
 var path = require('path'),
     parse_args = require('../commargs.js'),
     F = require('../../../build/filter.bundle.js'),
+    echo = console.log,
     args = parse_args(),
+    format = 'png',//(args.options['format'] || 'png').toLowerCase(),
     type = 'hor',//(args.options['type'] || 'hor').toLowerCase(),
     width = 300,//parseInt(args.options['width']||'100', 10),
     height = 521,//parseInt(args.options['height']||'100', 10),
+    fps = 12,
     imgs = [
-    path.join(__dirname,'./CapGuyWalk0001.png'),
-    path.join(__dirname,'./CapGuyWalk0002.png'),
-    path.join(__dirname,'./CapGuyWalk0003.png'),
-    path.join(__dirname,'./CapGuyWalk0004.png'),
-    path.join(__dirname,'./CapGuyWalk0005.png'),
-    path.join(__dirname,'./CapGuyWalk0006.png'),
-    path.join(__dirname,'./CapGuyWalk0007.png'),
-    path.join(__dirname,'./CapGuyWalk0008.png'),
-    path.join(__dirname,'./CapGuyWalk0009.png'),
-    path.join(__dirname,'./CapGuyWalk0010.png'),
-    path.join(__dirname,'./CapGuyWalk0011.png'),
-    path.join(__dirname,'./CapGuyWalk0012.png'),
-    path.join(__dirname,'./CapGuyWalk0013.png'),
-    path.join(__dirname,'./CapGuyWalk0014.png'),
-    path.join(__dirname,'./CapGuyWalk0015.png'),
-    path.join(__dirname,'./CapGuyWalk0016.png')
-    ],//(args.options['sprites'] || '').split(','),
-    format = 'png',//(args.options['format'] || 'png').toLowerCase(),
-    binaryManager, sprite;//, blend;
+    './CapGuyWalk0001.png',
+    './CapGuyWalk0002.png',
+    './CapGuyWalk0003.png',
+    './CapGuyWalk0004.png',
+    './CapGuyWalk0005.png',
+    './CapGuyWalk0006.png',
+    './CapGuyWalk0007.png',
+    './CapGuyWalk0008.png',
+    './CapGuyWalk0009.png',
+    './CapGuyWalk0010.png',
+    './CapGuyWalk0011.png',
+    './CapGuyWalk0012.png',
+    './CapGuyWalk0013.png',
+    './CapGuyWalk0014.png',
+    './CapGuyWalk0015.png',
+    './CapGuyWalk0016.png'
+    ]//(args.options['sprites'] || '').split(',')
+    //, blend = F.BlendFilter(new Array(5*imgs.length));
+;
 
-function start( sprite, i, imgs, width, height )
+function start( imMan, i, sprite, format, type, imgs, width, height, fps )
 {
-    if ( 0 === i ) console.log('Generating Animation..');
-    if ( i >= imgs.length ) { if ( 0 < imgs.length ) finish( sprite, imgs, width, height ); return; }
-    var img_file = imgs[i];
-    binaryManager.read( img_file, function( img ){
+    if ( 0 === i ) echo('Generating Animation..');
+    if ( i >= imgs.length )
+    {
+        if ( 0 < imgs.length ) finish( imMan, sprite, format, type, imgs, width, height, fps );
+        return;
+    }
+    var img_file = '.' === imgs[i].charAt(0) ? path.join(__dirname, imgs[i]) : imgs[i];
+    imMan.read( img_file, function( img ){
         imgs[i] = img;
         /*blend.setInput(i+1, img).setInputValues(i+1, {
                 mode: "normal",
@@ -43,20 +50,23 @@ function start( sprite, i, imgs, width, height )
                 startY: 0
         });*/
         sprite.paste(img, i*width, 0);
-        console.log('animation sprite: "' + img_file + '" pasted');
-        start( sprite, i+1, imgs, width, height );
+        echo('animation sprite: "' + img_file + '" pasted');
+        start( imMan, i+1, sprite, format, type, imgs, width, height, fps );
     }, function( err ){
         imgs[i] = null;
-        console.log('error while loading sprite image: ' + err);
-        start( sprite, i+1, imgs, width, height );
+        echo('error while loading sprite image: ' + err);
+        start( imMan, i+1, sprite, format, type, imgs, width, height, fps );
     });
 }
-function finish( sprite, imgs, width, height )
+function finish( imMan, sprite, format, type, imgs, width, height, fps )
 {
-    console.log('Output animation files..');
+    echo('Output animation files..');
     var sprite_img = './animation-sprite'+('jpg' === format?'.jpg':'.png');
     //blend.apply( sprite );
-    binaryManager.write( path.join(__dirname, sprite_img), sprite, function( ){
+    imMan.write(
+        path.join(__dirname, sprite_img), sprite,
+        function( ){
+        var nframes = imgs.length, dur = nframes/fps;
         F.IO.FileManager().responseType('text').write(path.join(__dirname,'./animation-sprite.html'), '\
 <!DOCTYPE html>\
 <html lang="en">\
@@ -68,45 +78,72 @@ function finish( sprite, imgs, width, height )
 \
 <style type="text/css">\
     .sprite-animation {\
-        width: '+width+'px;\
-        height: '+height+'px;\
         position: relative;\
         display: block;\
+        width: '+width+'px;\
+        height: '+height+'px;\
         overflow: hidden;\
-        background-position: 0px 0px;\
         background-image: url("'+sprite_img+'");\
-        -webkit-animation: spriteanimation 2s steps('+imgs.length+') infinite;\
-        -moz-animation: spriteanimation 2s steps('+imgs.length+') infinite;\
-        animation: spriteanimation 2s steps('+imgs.length+') infinite;\
+        background-position: 0px 0px;\
+    }\
+    .sprite-animation.animated {\
+        -webkit-animation-name: spriteanimation;\
+        -webkit-animation-duration: '+dur+'s;\
+        -webkit-animation-iteration-count: infinite;\
+        -webkit-animation-timing-function: steps('+nframes+');\
+        -moz-animation-name: spriteanimation;\
+        -moz-animation-duration: '+dur+'s;\
+        -moz-animation-iteration-count: infinite;\
+        -moz-animation-timing-function: steps('+nframes+');\
+        -ms-animation-name: spriteanimation;\
+        -ms-animation-duration: '+dur+'s;\
+        -ms-animation-iteration-count: infinite;\
+        -ms-animation-timing-function: steps('+nframes+');\
+        -o-animation-name: spriteanimation;\
+        -o-animation-duration: '+dur+'s;\
+        -o-animation-iteration-count: infinite;\
+        -o-animation-timing-function: steps('+nframes+');\
+        animation-name: spriteanimation;\
+        animation-duration: '+dur+'s;\
+        animation-iteration-count: infinite;\
+        animation-timing-function: steps('+nframes+');\
     }\
     @-webkit-keyframes spriteanimation {\
-        100% { background-position: -'+((imgs.length-1)*width)+'px 0px; }\
+        0% { background-position: 0px 0px; }\
+        100% { background-position: -'+(nframes*width)+'px 0px; }\
     }\
     @keyframes spriteanimation {\
-        100% { background-position: -'+((imgs.length-1)*width)+'px 0px; }\
+        0% { background-position: 0px 0px; }\
+        100% { background-position: -'+(nframes*width)+'px 0px; }\
     }\
 </style>\
 </head>\
 <body>\
-<div class="sprite-animation">&nbsp;</div>\
+<div class="sprite-animation animated">&nbsp;</div>\
 </body>\
 </html>\
 ', function(){
-            console.log('completed.');
+            echo('completed.');
         });
     }, function( err ){
-        console.log('error while writing sprite: ' + err);
+        echo('error while writing output sprite: ' + err);
     });
 }
 
 if ( imgs.length > 0 )
 {
-    binaryManager = 'jpg' === format ? F.IO.BinaryManager( F.Codec.JPG, {quality: 100} ) : F.IO.BinaryManager( F.Codec.PNG, {deflateLevel: 9} );
-    //blend = F.BlendFilter(new Array(5*imgs.length));
-    sprite = F.Image().restorable(false).createImageData(imgs.length*width, height);
-    start( sprite, 0, imgs, width, height );
+    start( 'jpg' === format ? F.IO.BinaryManager( F.Codec.JPG, {quality: 100} ) : F.IO.BinaryManager( F.Codec.PNG, {deflateLevel: 9} ),
+        0,
+        F.Image().restorable(false).createImageData(imgs.length*width, height),
+        format,
+        type,
+        imgs,
+        width,
+        height,
+        fps
+    );
 }
 else
 {
-    console.log('No sprite images!');
+    echo('No sprite images!');
 }
